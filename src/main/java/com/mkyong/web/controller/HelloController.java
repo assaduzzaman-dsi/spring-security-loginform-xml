ecc6ce4098e548fe472d22dfeb0e32e6485fcc9b
assad <assadice@yahoo.com>
2016-03-26 21:20:17 +0600

    added force logout from two different user agent

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -1,5 +1,11 @@
 package com.mkyong.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +14,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
+	
+	private static String _sessionId;
+	
+	@Autowired
+	private SessionRegistry sessionRegistry;
 
 	@RequestMapping(value = { "/", "/welcome**" }, method = RequestMethod.GET)
 	public ModelAndView welcomePage() {
 
+		System.out.println("session id needed to expire::::" + _sessionId);
+		
+		
+		if(null != _sessionId) {
+			SessionInformation sessionInfo = sessionRegistry.getSessionInformation(_sessionId);
+			System.out.println("session infor in session registry:::" + sessionInfo);
+			
+			if(null != sessionInfo) sessionInfo.expireNow();
+		}
+		
+		System.out.println("Logged in user expire:::");
 		ModelAndView model = new ModelAndView();
 		model.addObject("title", "Spring Security Custom Login Form");
 		model.addObject("message", "This is welcome page!");
@@ -21,8 +43,14 @@ public class HelloController {
 	}
 
 	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
-	public ModelAndView adminPage() {
+	public ModelAndView adminPage(HttpServletRequest request) {
 
+		
+		HttpSession session = request.getSession(false);
+		if(null == _sessionId) _sessionId = session.getId();
+		
+		System.out.println("set sessionId:::" + _sessionId);
+		
 		ModelAndView model = new ModelAndView();
 		model.addObject("title", "Spring Security Custom Login Form");
 		model.addObject("message", "This is protected page!");
@@ -34,11 +62,15 @@ public class HelloController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "expired", required = false) String expired,
 			@RequestParam(value = "logout", required = false) String logout) {
 
 		ModelAndView model = new ModelAndView();
 		if (error != null) {
 			model.addObject("error", "Invalid username and password!");
+		}
+		if (expired != null) {
+			model.addObject("expired", "Please login again!");
 		}
 
 		if (logout != null) {
--- src/main/webapp/WEB-INF/pages/login.jsp
+++ src/main/webapp/WEB-INF/pages/login.jsp
@@ -46,6 +46,10 @@
 		<c:if test="${not empty error}">
 			<div class="error">${error}</div>
 		</c:if>
+
+		<c:if test="${not empty expired}">
+			<div class="error">${expired}</div>
+		</c:if>
 		<c:if test="${not empty msg}">
 			<div class="msg">${msg}</div>
 		</c:if>
